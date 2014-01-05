5379edd8bc6739bc877b1198e43c7b425fd4a1e5
Becky <beckylashua@gmail.com>
2014-01-04 20:52:00 -0500

    Hw done

--- assignments/03/assignment01.js
+++ assignments/03/assignment01.js
@@ -6,37 +6,43 @@
 var dict = {
   a: true,
   b: false,
-  c: a || b,
+  c: a || b, 
   d: 1 < 10,
   e: 128;
 }
 
 var x = dict['e'];
-/* x = ? [5] */
+/* x = 128 [5] */
 
 var x = ((dict['a'] && dict['d']) || dict['b']);
-/* x = ? [10] */
+/* x = true [10] */
 
 var x = ((25 % 3 == 0) || (dict['e'] % 64)) && dict['c'];
-/* x = ? [15] */
+/* x = true [15] */
 
 /* 
  * [5] Add two different items to x
  */
-var x = [];
+dict[f] = 63 < 52;
+dict[g] = 10 % 2;
+
 
 /* 
  * [10] Create an array with two items where the indices are not numbers.
  */
-var x = /* CODE HERE */;
+var kittens = {
+  a: is,
+  b: cutes;
+} 
 
 /* 
  * [25] In the space provided show what would be printed to the console if 
  * this code is executed.
- */
+ * (nothing would be added, because we did not command it to print. We didn't use console.log(). 
+ * But as for the value of x, right now it stands at being "false"*/
  
 var x = 17,
-    y = ((true || false) || (false && false)) && !false,
+    y = ((true || false) || (false && false)) && !false, /* true*/
     z = "this is a string";
 
 if ('this is a string' == z) {
@@ -82,7 +88,10 @@ else {
 
 /*
  * Print the numbers in order as they would be if this code was executed:
- *
+ * 0
+ * 2
+ * 4
+ * 11
  */
  
 /* 
@@ -93,29 +102,43 @@ var i = 0;
 while (i < 32) {
   i++;
 }
+  for (var i = 0; i < 32; i++) {
+  }
+  
+  for (var i = 0; i < 32; ) {
+  i++;
+  }
+  
+  for (var i = 0; i++;) {
+    if (i >= 32)
+      break;
+  }
+  
+  
+
 
 /*
  * [5] Complete this forloop so that it executes exactly 16 times
  */
  
-for (var i = 0; /* PUT CODE HERE */; i++) {
+for (var i = 0; i < 16; i++) {
 
 }
 
 /*
  * [15] Complete this forloop so that it executes exactly 16 times 
  */
-for (var i = 0; /* PUT YOUR CODE HERE */; i = i + 2) {}
+for (var i = 0; i < 32; i = i + 2) {}
 
 /*
  * [10] Complete this forloop so that it executes exactly TEN times
  */
-for (/* PUT YOUR CODE HERE */; i < 8; i++) {}
+for (i = -2; i < 8; i++) {}
 
 /*
  * [15] Complete this forloop so that it executes exactly 16 times
  */
-for (var i = 0; i < 64; /* PUT YOUR CODE HERE */) {}
+for (var i = 0; i < 64; i = i + 4) {}
 
 /*
  * [5] How many times will this forloop executes?
@@ -126,12 +149,13 @@ for (var i = -4; i < 0; i++) {}
  * [10] How many times will this forloop executes?
  */
 for (var i = 0; i != 3; i++) {}
+3 times
 
 /*
  * [15] How many times will this forloop executes?
  */
 for (var i = 1; i % 3 != 0; i++) {}
-
+2
 /*
  * [15] What does the following code print? 
  */
@@ -140,3 +164,4 @@ for (var i = 0; j < 10; j++) {
   i = i + 10;
 }
 console.log(i);
+10 times. 
